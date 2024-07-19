ea9ea4e680dc81e1397df261e9abbca2a924ca26
Bryce Shirley <114733577+bryceshirley@users.noreply.github.com>
2024-07-19 19:48:07 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open('requirements.txt') as f:
 # setup
 setup(
     name='sciml_bench',
-    version=version,
+    version="1.2.0",
     packages=find_packages(),
     include_package_data=True,
     install_requires=requirements,
